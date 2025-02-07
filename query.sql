82591cf57def3a3c186fdd333c38bbb74619abea
snehamettu2407 <snehamettu2407@gmail.com>
2025-02-07 10:33:54 -0800

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,16 +1,29 @@
 SELECT 
     rpt_uti,
-    -- First, select distinct keys and then apply CONCAT_WS
-    (SELECT CONCAT_WS(', ', COLLECT_LIST(keys_uti)) 
-     FROM (SELECT DISTINCT keys_uti FROM table_name) AS distinct_keys) AS keys_uti,
-     
-    (SELECT CONCAT_WS(', ', COLLECT_LIST(keys_src_sys)) 
-     FROM (SELECT DISTINCT keys_src_sys FROM table_name) AS distinct_sys) AS keys_src_sys,
-
-    (SELECT CONCAT_WS(', ', COLLECT_LIST(msghdr_trd_clsftn)) 
-     FROM (SELECT DISTINCT msghdr_trd_clsftn FROM table_name) AS distinct_clsftn) AS alpha_trade_classfctn_ind,
-
+    CONCAT_WS(' ', COLLECT_LIST(DISTINCT keys_uti)) AS keys_uti,
+    CONCAT_WS(' ', COLLECT_LIST(DISTINCT keys_src_sys)) AS keys_src_sys,
+    CONCAT_WS(' ', COLLECT_LIST(DISTINCT msghdr_trd_clsftn)) AS alpha_trade_classfctn_ind,
     ts_pty1,
     ts_pty2
+FROM 
+(
+    SELECT DISTINCT 
+        ltrim(IF(instr(keys_uti, keys_uti_prefix) = 1
+            OR instr(keys_uti, amc_firm_lgl_enty_id) = 1
+            OR instr(keys_uti, amc_cpty_lgl_enty_id) = 1, keys_uti, 
+            keys_uti_prefix || keys_uti)) AS rpt_uti,
+        a.keys_uti,
+        keys_src_sys,
+        msghdr_trd_clsftn,
+        CASE 
+            WHEN keys_flow LIKE '%Citix%' THEN src_trd_pty1_id 
+            ELSE src_trd_pty2_id 
+        END AS ts_pty1,
+        CASE 
+            WHEN keys_flow LIKE '%Citix%' THEN src_trd_pty2_id 
+            ELSE src_trd_pty1_id 
+        END AS ts_pty2
+    FROM table_name
+) grouped_data
+GROUP BY rpt_uti, ts_pty1, ts_pty2;
 
-FROM table_name;
